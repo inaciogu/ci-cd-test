e3e690892d7adbfd693cb0b820fa0e3f1e6ff8b4
Gustavo <gus.inacio@hotmail.com>
2022-05-17 18:51:20 -0300

    chore: Start to build the main.yml file.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Esse app vai ser deployado utilizando CI/CD
         </a>
       </header>
     </div>
